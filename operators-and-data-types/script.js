b78909dbd18d8096522bae988f34b21480dc2789
Katharina Ephan <ephankatharina@gmail.com>
2023-02-01 16:19:21 +0100

    kata about operators and data types

---
+++ operators-and-data-types/script.js
@@ -0,0 +1,27 @@
+// 1
+
+console.log("K" + "a" + "t" + "h" + "a" + "r" + "i" + "n" + "a");
+
+// 2
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 >= 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" != 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 2 received 22
+console.log(+"2" + 2); //Ask about that tmrw bc what?
+
+// 3
+
+console.log(25 / 5 == 5 && 50 / 10 == 5); // Result: 5
